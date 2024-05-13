a7a9a84330febd06177963ca2cbd16681d39e95e
JamesGlik <sadoiani9900@gmail.com>
2024-05-14 00:27:47 +0400

    part 2
    finished part 2, added section about information about me, added hovers, JS, additional CSS, also fixed media query for mobile versions, proceeding to part 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,63 @@
 const photoCarousel = document.querySelector('.photo-carousel img');
 
-
 const imageSources = [
     'assets/images/person1',
     'assets/images/person2',
     'assets/images/person3'
 ];
 
-
 let currentImageIndex = 0;
 
-
 function changePhoto() {
-    
     currentImageIndex = (currentImageIndex + 1) % imageSources.length;
-
-    
     photoCarousel.src = imageSources[currentImageIndex];
 }
 
-
 setInterval(changePhoto, 5000);
 
+const progressBarFirstPage = document.querySelector('.first-page .progress-bar');
 
-const progressBar = document.querySelector('.progress-bar');
-
-
-function fillProgressBar() {
-    
+function fillFirstPageProgressBar() {
     const section = document.querySelector('.satisfaction-section');
     const sectionTop = section.getBoundingClientRect().top;
 
-    
     if (sectionTop <= window.innerHeight && sectionTop >= 0) {
-        
-        progressBar.style.width = '100%';
-        
-        window.removeEventListener('scroll', fillProgressBar);
+        progressBarFirstPage.style.width = '100%';
+        window.removeEventListener('scroll', fillFirstPageProgressBar);
     }
 }
 
+window.addEventListener('scroll', fillFirstPageProgressBar);
 
-window.addEventListener('scroll', fillProgressBar);
+const progressBarsSecondPage = document.querySelectorAll('.second-page .progress-bars');
+
+
+function fillProgressBars() {
+    const progressBars = document.querySelectorAll('.progress-bars');
+    progressBars.forEach(progressBar => {
+        const percentage = parseInt(progressBar.previousElementSibling.textContent.match(/\d+/)[0]);
+        const progressBarTop = progressBar.getBoundingClientRect().top;
+        const progressBarBottom = progressBar.getBoundingClientRect().bottom;
+        const viewportHeight = window.innerHeight;
+        
+        
+        let fillPercentage = 0;
+        if (progressBarTop < viewportHeight && progressBarBottom > 0) {
+            const progressBarHeight = progressBarBottom - progressBarTop;
+            const fillHeight = Math.min(progressBarHeight, viewportHeight - progressBarTop);
+            fillPercentage = (fillHeight / progressBarHeight) * percentage;
+        }
+        
+        progressBar.querySelector('.fill').style.width = `${fillPercentage}%`;
+    });
+}
+
+window.addEventListener('scroll', fillProgressBars);
+
+fillProgressBars();
+
+
+
+
+
+
